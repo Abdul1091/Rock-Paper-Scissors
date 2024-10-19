b6564849e03967f463dcc1381b57b3adb08717eb
Abdul1091 <111249085+Abdul1091@users.noreply.github.com>
2024-10-19 16:38:30 +0100

    Add playRound function

--- script.js
+++ script.js
@@ -22,3 +22,36 @@ function getHumanChoice() {
 }
 
 console.log(getHumanChoice());
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+// Function to play a single round
+function playRound(humanChoice, computerChoice) {
+
+    //humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    }
+
+    else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    )   {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    }
+
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+
+    console.log(`Scores => Human: ${humanScore}, Computer: ${computerScore}`);
+}
+
+console.log(playRound());
